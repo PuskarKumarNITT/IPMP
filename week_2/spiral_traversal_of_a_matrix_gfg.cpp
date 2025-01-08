b4f98c80466fecdab6679e821882479d1e93aa51
PuskarKumarNITT <106123100@nitt.edu>
2025-01-08 18:45:12 +0530

    spiral traversal of a matrix

---
+++ week_2/spiral_traversal_of_a_matrix_gfg.cpp
@@ -0,0 +1,61 @@
+// https://www.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
+        int n = mat.size(),m = mat[0].size();
+        vector<int> v;
+        int top = 0,left =0,right = m-1,bottom = n-1;
+        while(left <= right && top <= bottom){
+            for(int i=left ;i<= right;++i) v.push_back(mat[top][i]);
+            top++;
+            for(int i=top;i<=bottom;++i) v.push_back(mat[i][right]);
+            right--;
+            if(top <= bottom){
+                for(int i=right;i>=left;--i) v.push_back(mat[bottom][i]);
+                bottom--;
+            }
+            if(left <= right){
+                for(int i=bottom;i>=top;--i) v.push_back(mat[i][left]);
+                left++;
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
